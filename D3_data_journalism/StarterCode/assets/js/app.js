4fe02317f25cea80dc8fc6cb70d47a3e1db3c57b
Akriti Suri <akritisuri96@gmail.com>
2020-12-14 18:31:19 -0800

    three

--- D3_data_journalism/StarterCode/assets/js/app.js
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -10,3 +10,20 @@ var margin = {
     bottom: 60, 
     left: 60
 };
+
+// Define the dimensions 
+var chartWidth = svgWidth - margin.left - margin.right;
+var chartHeight = svgHeight - margin.top - margin.bottom; 
+
+// Select the body, append SVG area, and set dimensions 
+var svg = d3.select("scatter")
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight); 
+
+// append group are and set its margins 
+var ChartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+var file = "StarterCode/assets/data/data.csv" 
+d3.csv(file)
